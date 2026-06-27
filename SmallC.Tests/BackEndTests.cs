62b7881ae03298c9e2f2d60a35754466e9e41fc8
Nathaniel Bond <nabond251@gmail.com>
2026-06-27 11:21:19 -0400

    Make initial implementation

--- SmallC.Tests/BackEndTests.cs
+++ SmallC.Tests/BackEndTests.cs
@@ -4,6 +4,8 @@
 
 namespace SmallC.Tests;
 
+using SmallC.Cc4;
+
 /// <summary>
 /// Tests the back end functions.
 /// </summary>
@@ -12,9 +14,38 @@ public class BackEndTests
     /// <summary>
     /// Tests that header is generated.
     /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
     [Fact]
-    public void GeneratesHeader()
+    public async Task GeneratesHeader()
     {
-        Assert.Fail();
+        var outputStream = new MemoryStream();
+        var output = new StreamWriter(outputStream);
+        var sut = new BackEnd(output);
+
+        await sut.HeaderAsync();
+        await output.FlushAsync();
+        outputStream.Position = 0;
+        var reader = new StreamReader(outputStream);
+        var actual = await reader.ReadToEndAsync();
+
+        var expected = @"CODE SEGMENT PUBLIC
+ASSUME CS:CODE, SS:DATA, DS:DATA
+extrn __eq: near
+extrn __ne: near
+extrn __le: near
+extrn __lt: near
+extrn __ge: near
+extrn __gt: near
+extrn __ule: near
+extrn __ult: near
+extrn __uge: near
+extrn __ugt: near
+extrn __lneg: near
+extrn __switch: near
+dw 0
+CODE ENDS
+DATA SEGMENT PUBLIC
+dw 0";
+        Assert.Equal(expected, actual);
     }
 }
---
+++ SmallC/Cc4/BackEnd.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="BackEnd.cs" company="Soli Deo Gloria Apps">
+// Copyright (c) Soli Deo Gloria Apps. All rights reserved.
+// </copyright>
+
+namespace SmallC.Cc4;
+
+/// <summary>
+/// Back end.
+/// </summary>
+public class BackEnd(StreamWriter output)
+{
+    /// <summary>
+    /// Print all assembler info before any code is generated
+    /// and ensure that the segments appear in the correct order.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public async Task HeaderAsync()
+    {
+        await output.WriteAsync(@"CODE SEGMENT PUBLIC
+ASSUME CS:CODE, SS:DATA, DS:DATA
+extrn __eq: near
+extrn __ne: near
+extrn __le: near
+extrn __lt: near
+extrn __ge: near
+extrn __gt: near
+extrn __ule: near
+extrn __ult: near
+extrn __uge: near
+extrn __ugt: near
+extrn __lneg: near
+extrn __switch: near
+dw 0
+CODE ENDS
+DATA SEGMENT PUBLIC
+dw 0");
+    }
+}
