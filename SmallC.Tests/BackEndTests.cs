62a3bfcb9b8695bc5bef8c090d474b41d36e6db8
Nathaniel Bond <nabond251@gmail.com>
2026-06-27 11:01:21 -0400

    Fix build errors

---
+++ SmallC.Tests/BackEndTests.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="BackEndTests.cs" company="Soli Deo Gloria Apps">
+// Copyright (c) Soli Deo Gloria Apps. All rights reserved.
+// </copyright>
+
+namespace SmallC.Tests;
+
+/// <summary>
+/// Tests the back end functions.
+/// </summary>
+public class BackEndTests
+{
+    /// <summary>
+    /// Tests that header is generated.
+    /// </summary>
+    [Fact]
+    public void GeneratesHeader()
+    {
+        Assert.Fail();
+    }
+}
