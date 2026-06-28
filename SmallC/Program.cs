7b7d1c7f921b914e4bdc0c70872dd45d8c972e30
Nathaniel Bond <nabond251@gmail.com>
2026-06-27 23:02:04 -0400

    Play around with externs

--- SmallC/Program.cs
+++ SmallC/Program.cs
@@ -5,6 +5,12 @@
 using SmallC.Cc;
 using SmallC.Cc4;
 
-var storage = new Storage(new([], []));
+var storage = new Storage(new(
+    [],
+    [
+        new(SymbolTableEntry.SymbolIdentity.Function, SymbolTableEntry.SymbolType.Int, SymbolTableEntry.SymbolClass.AutoExt, 2, null, "func"),
+        new(SymbolTableEntry.SymbolIdentity.Function, SymbolTableEntry.SymbolType.Int, SymbolTableEntry.SymbolClass.Static, 2, null, "main"),
+    ]));
 var backend = new BackEnd(storage, Console.Out);
 await backend.HeaderAsync().ConfigureAwait(true);
+await backend.TrailerAsync().ConfigureAwait(true);
