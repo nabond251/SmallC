7a0a664ab4924be9771b0e1f158a38878068ee64
Nathaniel Bond <nabond251@gmail.com>
2026-07-05 15:48:37 -0400

    Continue adding scanning use cases

--- SmallC.Cc2/ScanningUseCases.cs
+++ SmallC.Cc2/ScanningUseCases.cs
@@ -9,8 +9,59 @@ using SmallC.Cc;
 /// <summary>
 /// Scanning use cases.
 /// </summary>
-public class ScanningUseCases(Storage storage)
+public class ScanningUseCases(
+    InputUseCases input,
+    Storage storage)
 {
+    /// <summary>
+    /// Advances the input past white space to the beginning of the next token
+    /// or until the end of the input is reached.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public async Task BlanksAsync()
+    {
+        while (true)
+        {
+            while (storage.Ch.HasValue)
+            {
+                if (this.White())
+                {
+                    _ = this.Gch();
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            if (storage.Line == storage.MLine)
+            {
+                return;
+            }
+
+            await input.PreprocessAsync().ConfigureAwait(false);
+            if (storage.Eof)
+            {
+                break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns <c>true</c> if the current input character is a space or a
+    /// control character and <c>false</c> otherwise.
+    /// </summary>
+    /// <returns>
+    /// A value indicating wether the current input character is a space or a
+    /// control character.
+    /// </returns>
+    public bool White()
+    {
+        return
+            storage.LPtr < storage.Line?.Length &&
+            storage.Line?[storage.LPtr] <= ' ';
+    }
+
     /// <summary>
     /// Returns the current character of the input line, advances
     /// <see cref="Storage.LPtr"/> to the next one, and places it in
