990d89681250a75a6e31a40eb238161d4ed3db1c
Nathaniel Bond <nabond251@gmail.com>
2026-07-21 19:24:19 -0400

    Add fputs

---
+++ SmallC/FPUTS.C
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "clib.h"
+/*
+** Write a string to fd.
+** Entry: string = pointer to null-terminated string.
+**        fd     = File descriptor of pertinent file.
+*/
+fputs(string, fd) char *string; int fd; {
+  while(*string) fputc(*string++, fd) ;
+  }
