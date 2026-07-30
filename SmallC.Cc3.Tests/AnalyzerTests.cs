4ab64cd9b26098c27db98415e248f22735a9361e
Nathaniel Bond <nabond251@gmail.com>
2026-07-30 18:32:49 -0400

    Assert state

--- SmallC.Cc3.Tests/AnalyzerTests.cs
+++ SmallC.Cc3.Tests/AnalyzerTests.cs
@@ -23,21 +23,39 @@ public class AnalyzerTests
     /// <param name="pLine">Parsing buffer.</param>
     /// <param name="lPtr">Index to <see cref="Storage.Line"/>.</param>
     /// <param name="expected">Expected character literal.</param>
+    /// <param name="expectedCh">
+    /// Expected current character of input line.
+    /// </param>
+    /// <param name="expectedNCh">Expected next character of input line.</param>
+    /// <param name="expectedLPtr">
+    /// Expected index to <see cref="Storage.Line"/>.
+    /// </param>
     [Theory]
-    [InlineData(null, null, "", 0, null)]
-    [InlineData('a', null, "a", 0, 'a')]
-    [InlineData(null, null, "a", 1, null)]
-    [InlineData(' ', 'a', " a", 0, ' ')]
-    [InlineData('a', null, " a", 1, 'a')]
-    [InlineData(null, null, " a", 2, null)]
+    [InlineData(null, null, "", 0, null, null, null, 0)]
+    [InlineData('a', null, "a", 0, 'a', null, null, 1)]
+    [InlineData(null, null, "a", 1, null, null, null, 1)]
+    [InlineData(' ', 'a', " a", 0, ' ', 'a', null, 1)]
+    [InlineData('a', null, " a", 1, 'a', null, null, 2)]
+    [InlineData(null, null, " a", 2, null, null, null, 2)]
     public void GetsCharacterLiteral(
-        char? ch, char? nCh, string pLine, int lPtr, char? expected)
+        char? ch,
+        char? nCh,
+        string pLine,
+        int lPtr,
+        char? expected,
+        char? expectedCh,
+        char? expectedNCh,
+        int expectedLPtr)
     {
-        var (sut, _) = Arrange(ch: ch, nCh: nCh, pLine: pLine, lPtr: lPtr);
+        var (sut, storage) =
+            Arrange(ch: ch, nCh: nCh, pLine: pLine, lPtr: lPtr);
 
         var actual = sut.LitChar();
 
         Assert.Equal(expected, actual);
+        Assert.Equal(expectedCh, storage.Ch);
+        Assert.Equal(expectedNCh, storage.NCh);
+        Assert.Equal(expectedLPtr, storage.LPtr);
     }
 
     private static (Analyzer Sut, Storage Storage) Arrange(
