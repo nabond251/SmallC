2e88fa1014de1ac5f947f6b736ba5d068f4f9fe3
Nathaniel Bond <nabond251@gmail.com>
2026-07-30 21:50:23 -0400

    Remove char lit

--- SmallC.Cc3.Tests/AnalyzerTests.cs
+++ SmallC.Cc3.Tests/AnalyzerTests.cs
@@ -18,60 +18,6 @@ using static SmallC.Cc.SymbolTableEntry;
 public class AnalyzerTests
 {
     /// <summary>
-    /// Tests that can get character literal.
-    /// </summary>
-    /// <param name="ch">Current character of input line.</param>
-    /// <param name="nCh">Next character of input line.</param>
-    /// <param name="pLine">Parsing buffer.</param>
-    /// <param name="lPtr">Index to <see cref="Storage.Line"/>.</param>
-    /// <param name="expected">Expected character literal.</param>
-    /// <param name="expectedCh">
-    /// Expected current character of input line.
-    /// </param>
-    /// <param name="expectedNCh">Expected next character of input line.</param>
-    /// <param name="expectedLPtr">
-    /// Expected index to <see cref="Storage.Line"/>.
-    /// </param>
-    [Theory]
-    [InlineData(null, null, "", 0, null, null, null, 0)]
-    [InlineData('a', null, "a", 0, 'a', null, null, 1)]
-    [InlineData(null, null, "a", 1, null, null, null, 1)]
-    [InlineData(' ', 'a', " a", 0, ' ', 'a', null, 1)]
-    [InlineData('a', null, " a", 1, 'a', null, null, 2)]
-    [InlineData(null, null, " a", 2, null, null, null, 2)]
-    [InlineData('\\', null, "\\", 0, '\\', null, null, 1)]
-    [InlineData('\\', 'n', "\\n", 0, '\n', null, null, 2)]
-    [InlineData('\\', 't', "\\t", 0, '\t', null, null, 2)]
-    [InlineData('\\', 'b', "\\b", 0, '\b', null, null, 2)]
-    [InlineData('\\', 'f', "\\f", 0, '\f', null, null, 2)]
-    [InlineData('\\', '0', "\\0", 0, '\0', null, null, 2)]
-    [InlineData('\\', '1', "\\1", 0, (char)1, null, null, 2)]
-    [InlineData('\\', '1', "\\9", 0, '9', null, null, 2)]
-    [InlineData('\\', '1', "\\12", 0, (char)10, null, null, 3)]
-    [InlineData('\\', '1', "\\123", 0, (char)83, null, null, 4)]
-    [InlineData('\\', '1', "\\1234", 0, (char)83, '4', null, 4)]
-    public void GetsCharacterLiteral(
-        char? ch,
-        char? nCh,
-        string pLine,
-        int lPtr,
-        char? expected,
-        char? expectedCh,
-        char? expectedNCh,
-        int expectedLPtr)
-    {
-        var (sut, storage) =
-            Arrange(ch: ch, nCh: nCh, pLine: pLine, lPtr: lPtr);
-
-        var actual = sut.LitChar();
-
-        Assert.Equal(expected, actual);
-        Assert.Equal(expectedCh, storage.Ch);
-        Assert.Equal(expectedNCh, storage.NCh);
-        Assert.Equal(expectedLPtr, storage.LPtr);
-    }
-
-    /// <summary>
     /// Tests that can parse character constant.
     /// </summary>
     /// <param name="inputText">Input stream text.</param>
