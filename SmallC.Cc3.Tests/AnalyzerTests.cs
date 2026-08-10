2db7bba8070a9eebd17d35ef4e0093172ce7dc39
Nathaniel Bond <nabond251@gmail.com>
2026-08-10 19:04:18 -0400

    Test bool and with dropout

--- SmallC.Cc3.Tests/AnalyzerTests.cs
+++ SmallC.Cc3.Tests/AnalyzerTests.cs
@@ -121,6 +121,24 @@ null, null, null, null, null, null, null, null, SymbolType.Int, 2, null, null,
 SymbolIdentity.Variable, SymbolType.Int, SymbolClass.Automatic, 2, 2, "i", SymbolType.Int, null, null, 0, null, null,
 @"LEA AX,2[BP]
 ", "")]
+    [InlineData("i && 2", false,
+null, null, null, null, null, null, null, null, null, 0, null, null,
+@"LEA AX,2[BP]
+MOV BX,AX
+MOV AX,[BX]
+OR AX,AX
+JNE $+5
+JMP _1
+MOV AX,2
+OR AX,AX
+JNE $+5
+JMP _1
+MOV AX,1
+JMP _2
+_1:
+XOR AX,AX
+_2:
+", "")]
     [InlineData("i | 2", false,
 SymbolIdentity.Variable, SymbolType.Int, SymbolClass.Automatic, 2, 2, "i", SymbolType.Int, null, null, 0, PCode.OR12, null,
 @"LEA AX,2[BP]
